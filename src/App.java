583bdf8fec389806cbc10e0d63be1ab1e35fbefd
Fekete Gergely <feketegergely062@gmail.com>
2023-02-20 13:44:29 +0100

    kesz

--- src/App.java
+++ src/App.java
@@ -12,17 +12,18 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.print("Keszitette: Fekete Gergely, I/1/N 2023 02 20\n ");
-        Scanner myObj = new Scanner(System.in);  
+        System.out.println("Feladat: 0358");
+        System.out.print("Keszitette: Fekete Gergely, I/1/N 2023-02-20\n ");
+        Scanner amugy = new Scanner(System.in);  
         System.out.println("Add meg a paralepid A oldalat");
 
-        double Aold = myObj.nextDouble();
+        double Aold = amugy.nextDouble();
         System.out.println("Add meg a paralepid B oldalat");
 
-        double Bold = myObj.nextDouble();
+        double Bold = amugy.nextDouble();
         System.out.println("Add meg a paralepid C oldalat");
 
-        double Cold = myObj.nextDouble();
+        double Cold = amugy.nextDouble();
         
         double terfogat = Aold*Bold*Cold;
     
